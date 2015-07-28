f15205085e044605795fc2f07fe28c89c2d14de4
Edwin Webb <edwin.webb@razorfish.com>
2015-07-28 12:38:56 +0100

    Add a really basic test

---
+++ test/test.js
@@ -0,0 +1,17 @@
+var assert = require("assert");
+
+describe('Sanity', function() {
+  describe('Tests Run', function () {
+    it('should pass easily', function () {
+      assert.equal(true, 1 === 1);
+    });
+  });
+});
+
+describe('Reactman', function() {
+  describe('Tests Run', function () {
+    it('should pass easily', function () {
+      assert.equal(true, 1 === 1);
+    });
+  });
+});
